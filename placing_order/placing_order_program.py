9b9edb5bbf20fe34c829cf9c4d108e6fca4c0da4
Chetan-Goyal <harshrock616@gmail.com>
2019-12-25 23:40:20 +0530

    Major Improvements in code of placing_order

--- placing_order/placing_order_program.py
+++ placing_order/placing_order_program.py
@@ -1,18 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtCore import Qt
 from placing_order import *
-
-# ** Main Tasks 
-# TODO: 1. Updating all three LCD at time of deletion of one product
-# TODO: 2. Testing
-# TODO: 3. 0 qty list add should be 0 but it is for 1
 
 class myForm(QDialog):
     cart_elements = {
-        'Pasta Sauce':0,
-        '2 x Oranges':0,
-        'Lays Chips (Spicy)':0,
-        'Coconut Biscuits':0
+        'Pasta Sauce            ':0,
+        '2 x Oranges            ':0,
+        'Lays Chips (Spicy)     ':0,
+        'Coconut Biscuits       ':0
     }
 
     def __init__(self):
@@ -20,45 +16,81 @@ class myForm(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.ui.add_product.clicked.connect(self.cart_add)
-        # self.ui.edit.connect(self.cart_edit)
+        self.ui.edit.clicked.connect(self.cart_edit)
         self.ui.Del.clicked.connect(self.cart_delete)
-        # self.ui.delete_all.connect(self.cart_delete_all)
+        self.ui.Del_all.clicked.connect(self.cart_delete_all)
         self.show()
     
     def cart_add(self):
-        product_details = self.ui.CB.currentText()
+        product_details = self.ui.CB.currentText().partition('-> $ ')
         qty = self.ui.SB.value()
-        price = float(product_details.split()[-1])
-        cost = price*qty
-        discount = round(price/10,2)*qty
-        final_price = price - discount
-        product_name = product_details.split('->')[0].strip()
-        myForm.cart_elements[product_name] += qty
-        qty = myForm.cart_elements[product_name]
-        
-        # if myForm.cart_elements[product_name] > 1:
-        #     self.ui.cart.takeItem(self.ui.cart.item(0))
-        list_record = f'{product_name}      {price}   {myForm.cart_elements[product_name]}    {price*myForm.cart_elements[product_name]}'
-        self.cart_update(product_name, list_record)
 
+        if not qty:        # checking if no item has to be added in list(when quantity = 0)
+            return None
+
+        price = float(product_details[-1])
+        cost = price*qty
+        discount = round((price/10)*qty,2)
+        final_price = cost - discount
+        product_name = product_details[0]
+        myForm.cart_elements[product_name] += qty
+        new_qty = myForm.cart_elements[product_name]
+        
+        # Deletes old record if same item is added again
+        if myForm.cart_elements[product_name] > new_qty-qty:
+            old_list_record = f'{product_name}{price}   {new_qty-qty}      {(new_qty-qty)*price}'
+            for i in range(self.ui.cart.count()): # finding index of old record in list
+                if self.ui.cart.item(i).text() == old_list_record:
+                    self.ui.cart.takeItem(i)  # deleting old list item at found position
+                    break
+        
+        # List with updated details
+        list_record = f'{product_name}{price}   {myForm.cart_elements[product_name]}      {price*myForm.cart_elements[product_name]}'
+
+        # Adding Item in list and updating total,display and pay lcd counter 
         self.ui.cart.addItem(list_record)
         self.ui.LCD_total.display( self.ui.LCD_total.value() + cost)
         self.ui.LCD_discount.display(self.ui.LCD_discount.value() + discount)
         self.ui.LCD_to_pay.display(self.ui.LCD_to_pay.value() + final_price)
 
-    def cart_update(self, product_name, record):
-        product_names = myForm.cart_elements.keys()
-        for index in range(self.ui.cart.count()):
-            if self.ui.cart.item(index).text().split()[0] in product_name:
-                self.ui.cart.setCurrentItem(record) # self.ui.cart.item(index).text())
-                break
-    
-    def cart_delete(self):
-        myForm.cart_elements[self.ui.cart.currentItem().text().split('  ')[0].strip()] = 0
 
-        print(myForm.cart_elements)
+    def cart_edit(self):
+        cart_item = self.ui.cart.currentItem().text()
+
+        self.ui.SB.setValue(int(cart_item.split()[-2]))
+
+        item = cart_item[:23] + '-> $ ' + cart_item.split()[-3]
+        index = self.ui.CB.findText(item, Qt.MatchFixedString)
+        print(item)
+        print(index)
+        if index >= 0:
+            self.ui.CB.setCurrentIndex(index)
+
+        self.cart_delete()
+
+
+    def cart_delete(self):
+        cart_item = self.ui.cart.currentItem().text().split()
+        total_price = float(cart_item[-1])
+
+        self.ui.LCD_total.display( self.ui.LCD_total.value() - total_price )
+        self.ui.LCD_discount.display(self.ui.LCD_discount.value() - total_price*0.1 )
+        self.ui.LCD_to_pay.display(self.ui.LCD_to_pay.value() - total_price*0.9 )
+
+        # setting quantity of current item in our dictionary to zero
+        myForm.cart_elements[self.ui.cart.currentItem().text()[0:24]] = 0
+
+        # deleting current item from list
         self.ui.cart.takeItem( self.ui.cart.currentRow() )
-    
+
+
+    def cart_delete_all(self):
+        self.ui.cart.clear()
+        self.ui.LCD_total.display(0)
+        self.ui.LCD_discount.display(0)
+        self.ui.LCD_to_pay.display(0)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = myForm()
